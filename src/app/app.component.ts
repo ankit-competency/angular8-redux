2b8be618c087cb82fdcafa8b9ad831b42121f368
Ankit Gupta <ankit.gupta@ranosys.com>
2019-09-24 15:05:20 +0530

    setup Redux and implement currency conversion API.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AmountChangeAction } from '../actions/amount';
+import { CurrenciesUpdateAction } from '../actions/currency';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../reducers';
+import { Observable } from 'rxjs';
+import { Currency } from '../models/currency';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-demo-app';
+  public amount$: Observable<number>;
+  public currencyRates$: Observable<Currency[]>;
+
+  constructor(public store: Store<fromRoot.State>) {
+    this.amount$ = store.select(fromRoot.getAmountState);
+    this.currencyRates$ = store.select(fromRoot.getCurrnecyRates);
+  }
+
+  // Dispatch the Action
+  ngOnInit() {
+    this.store.dispatch(new CurrenciesUpdateAction());
+  }
+
+  onAmountChange(amount: string) {
+    const number1 = parseFloat(amount);
+    if (!isNaN(number1)) {
+      this.store.dispatch(new AmountChangeAction(number1));
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from '../reducers/index';
 
+
+import { CurrencyService } from '../services/currency.service';
+import { CurrencyEffects } from '../effects/currencyEffects';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { EffectsModule } from '@ngrx/effects';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([CurrencyEffects])
   ],
-  providers: [],
+  providers: [CurrencyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
